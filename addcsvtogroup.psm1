9cbddc979445bc346e0fbd8368ebf3a9ef85ee7c
elightcap <50679504+elightcap@users.noreply.github.com>
2021-03-09 14:52:10 -0500

    fixed

--- addcsvtogroup.psm1
+++ addcsvtogroup.psm1
@@ -16,13 +16,14 @@ function add-csvtogroup {
         "Content-Type"  = "application/json"
         "Authorization" = $key
     }
-
+    $csv = import-csv -Path $csvpath
     foreach ($user in $csv) {
+        write-host $user.email
         $email = $user.email
-        $uProfile = invoke-restmethod "https://weedmpas.okta.com/api/v1/users?q=$email" -method get -Headers $headers
+        $uProfile = invoke-restmethod "https://weedmaps.okta.com/api/v1/users?q=$email" -method get -Headers $headers -verbose
         $uID = $uProfile.id
-        $gprofile = invoke-restmethod "https://weedmaps.okta.com/api/v1/groups?q=$group" -method get -Headers $headers
+        $gprofile = invoke-restmethod "https://weedmaps.okta.com/api/v1/groups?q=$group" -method get -Headers $headers -verbose
         $gID = $gprofile.id
-        invoke-restmethod -Uri "https://weedmaps.okta.com/api/v1/groups/$gid/users/$uId" -method put -Headers $headers
+        invoke-restmethod -Uri "https://weedmaps.okta.com/api/v1/groups/$gid/users/$uId" -method put -Headers $headers -verbose
     }
 }
